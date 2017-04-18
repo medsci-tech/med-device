c3856005a62f31df313134fe4c37a3db86c9e2fe
lostofland <772932587@qq.com>
2017-04-18 23:38:20 +0800

    add route

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::group(['middleware' => 'web'], function () {
-    //Auth::routes();
+    Auth::routes();
     Route::any('login', 'Auth\AuthController@login');
     Route::get('logout', 'Auth\AuthController@logout');
     Route::any('register', 'Auth\AuthController@register');
@@ -67,7 +67,7 @@ Route::group(['middleware' => 'web', 'namespace' => 'Web'], function () {
         Route::get('/', 'SearchController@index'); // 搜索相关
 
     });
-    Route::group(['prefix' => 'personal', 'namespace' => 'Personal'], function () {
+    Route::group(['prefix' => 'personal', 'namespace' => 'Personal','middleware' => 'auth'], function () {
 
         Route::get('/', 'PersonalController@index');
         Route::get('/collection', 'PersonalController@collection');
