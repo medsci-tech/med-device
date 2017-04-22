66e766c1618330ac5b506c86fc7d89de15d877b3
lostofland <772932587@qq.com>
2017-04-23 00:43:03 +0800

    限制提醒

--- resources/views/web/agent/index.blade.php
+++ resources/views/web/agent/index.blade.php
@@ -4,8 +4,10 @@
 
 @section('page_css')
 <link rel="stylesheet" type="text/css" href="style/broker.css">
+<link rel="stylesheet" type="text/css" href="/js/sweetalert/sweetalert.css">
 @endsection
-
+<script src="http://libs.baidu.com/jquery/1.7.2/jquery.min.js"></script>
+<script src="/js/sweetalert/sweetalert.min.js"></script>
 @section('content')
 	<div class="row nav">
 		<div class="col-md-offset-1 col-md-11">
@@ -23,12 +25,20 @@
 		<div class="shielder"></div>
 		<div class="mid-content">
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.</p>
-			<a class="reserve" href="{{ url('agent/agent-sign') }}">成为药械经纪人</a>
+			<a class="reserve" @if (!Auth::guest()) href="{{ url('agent/agent-sign') }}" @endif>成为药械经纪人</a>
 		</div>
 	</div>
 @endsection
 
 @section('page_js')
 <script src="/js/broker.js"></script>
+<script>
+    //弹出面板
+    $('.reserve').on('click', function () {
+        @if (Auth::guest())
+        sweetAlert("您还没有登录!");
+		@endif
+    });
+    </script>
 @endsection
 
