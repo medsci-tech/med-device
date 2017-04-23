36cedcc0e4b7dc91c100d4a41b95f319288a78f0
lostofland <772932587@qq.com>
2017-04-23 22:19:37 +0800

    个人中心

--- routes/web.php
+++ routes/web.php
@@ -67,7 +67,7 @@ Route::group(['middleware' => 'web', 'namespace' => 'Web'], function () {
 
     });
 	
-    Route::group(['prefix' => 'personal', 'namespace' => 'Personal'], function () {
+    Route::group(['prefix' => 'personal', 'namespace' => 'Personal','middleware' => 'auth'], function () {
 
         Route::get('/', 'PersonalController@index');
         Route::get('/collection', 'PersonalController@collection');
