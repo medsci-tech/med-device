a97fdbf4a2ce99fcc2bc476b4a22674806ceacd7
lostofland <772932587@qq.com>
2017-04-23 16:13:32 +0800

    登记限制

--- app/Http/Controllers/Web/Agent/AgentController.php
+++ app/Http/Controllers/Web/Agent/AgentController.php
@@ -33,9 +33,11 @@ class AgentController extends Controller
 
             $data = $request->all();
             $result =$this->checkAgent($request->all());
-            if(\Auth::user()->is_agent)
-                return response()->json(['code' => 200, 'status' => 0, 'message' => '您已经登记过了!']);
-
+            if (\Auth::check())
+            {
+                if(\Auth::user()->is_agent)
+                    return response()->json(['code' => 200, 'status' => 0, 'message' => '您已经登记过了!']);
+            }
             $data['user_id'] = \Auth::id();
             try {
                 if($result['status'] ==1)
