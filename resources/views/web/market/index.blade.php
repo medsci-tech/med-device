13586ab6cabb599b8f82ee46ef38107b468b613b
Kitsune <madfox.fw@gmail.com>
2017-04-25 15:54:53 +0800

    html 结构调整

--- resources/views/web/market/index.blade.php
+++ resources/views/web/market/index.blade.php
@@ -6,25 +6,24 @@
 <link rel="stylesheet" type="text/css" href="/style/service.css">
 @endsection
 @section('content')
-	<div class="row nav">
-		<div class="col-md-offset-1 col-md-11">
-			<a href="/">&nbsp;&nbsp;首&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;页&nbsp;&nbsp;</a>
-			<a href="product">药械产品招商</a>
-			<a class="focus">
-				药械营销服务
-				<div class="strip"></div>
-			</a>
-			<a href="agent">药械经纪人</a>
-		</div>
+<div class="row nav">
+	<div class="container">
+		<a href="/">&nbsp;&nbsp;首&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;页&nbsp;&nbsp;</a>
+		<a href="product">药械产品招商</a>
+		<a class="focus">
+			药械营销服务
+			<div class="strip"></div>
+		</a>
+		<a href="agent">药械经纪人</a>
 	</div>
+</div>
 
-	<div class="row mid">
-		<div class="shielder"></div>
-		<div class="mid-content">
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.</p>
-			<a class="reserve" href="{{ url('market/marketing-order/') }}" style="cursor: pointer">预约营销服务</a>
-		</div>
+<div class="row mid">
+	<div class="shielder"></div>
+	<div class="mid-content">
+		<a class="reserve" href="{{ url('market/marketing-order/') }}" style="cursor: pointer">预约营销服务</a>
 	</div>
+</div>
 @endsection
 
 @section('page_js')
